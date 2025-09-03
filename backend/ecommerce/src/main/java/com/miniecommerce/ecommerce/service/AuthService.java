5dd1482b625c1bc729d70994b18988fdba59a263
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-09-03 21:34:10 +0300

    update: service using dto for clean code

--- backend/ecommerce/src/main/java/com/miniecommerce/ecommerce/service/AuthService.java
+++ backend/ecommerce/src/main/java/com/miniecommerce/ecommerce/service/AuthService.java
@@ -1,6 +1,8 @@
 package com.miniecommerce.ecommerce.service;
 
 
+import com.miniecommerce.ecommerce.dto.AuthRequest;
+import com.miniecommerce.ecommerce.dto.RegisterRequest;
 import com.miniecommerce.ecommerce.model.User;
 import com.miniecommerce.ecommerce.security.JwtService;
 import lombok.RequiredArgsConstructor;
@@ -10,30 +12,39 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.security.core.AuthenticationException;
 
+
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+
     private final UserService userService;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    // Register user and return JWT
-    public String register(String username, String password, String role) {
-        User user = userService.registerUser(username, password, role);
+    // Register
+    public String register(RegisterRequest request) {
+        User user = userService.registerUser(
+                request.username(),
+                request.password(),
+                request.role()
+        );
         return jwtService.generateToken(user.getEmail());
     }
 
-    // Login and return JWT
-    public String login(String username, String password) {
+    // Login
+    public String login(AuthRequest request) {
         try {
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(username, password)
+                    new UsernamePasswordAuthenticationToken(
+                            request.username(),
+                            request.password()
+                    )
             );
         } catch (AuthenticationException e) {
             throw new RuntimeException("Invalid username or password");
         }
 
-        User user = userService.findByEmail(username)
+        User user = userService.findByEmail(request.username())
                 .orElseThrow(() -> new UsernameNotFoundException("Invalid username or password"));
         return jwtService.generateToken(user.getEmail());
     }

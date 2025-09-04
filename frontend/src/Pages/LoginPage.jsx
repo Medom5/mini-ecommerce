b99de8f403dd6bf3c27fd7ee651d56b2917b1275
Medom05 <145607268+Medom5@users.noreply.github.com>
2025-09-04 16:55:10 +0300

    feat: add input field login page

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,11 +1,36 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
-import React from "react";
 
 const LoginPage = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+
+
     return (
         <div>
             <h2>Login Page</h2>
+
+            <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+            />
+
+            <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+            />
         </div>
     );
 };
